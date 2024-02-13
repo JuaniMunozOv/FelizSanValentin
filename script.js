9144c595cc0d572714ca832589378801fee406f5
Juan Ignacio Muñoz <108429659+JuaniMunozOv@users.noreply.github.com>
2024-02-12 23:45:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-onload = () => {
-  document.body.classList.remove("container");
-};
-
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('iconoCarta').addEventListener('click', function() {
-    window.location.href = 'carta.html';
-  });
+    document.getElementById('iconoCarta').addEventListener('click', function() {
+      window.location.href = 'carta.html';
+    });
 });
+
+onload = () => {
+    document.body.classList.remove("container");
+  };
